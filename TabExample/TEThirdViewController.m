62c0e3019a61640978dbad5d188ddccfcaa7340f
Kerouz <luc.lequiniat@me.com>
2013-08-18 11:36:54 +1000

    Setting up third panel

---
+++ TabExample/TEThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  TEThirdViewController.m
+//  TabExample
+//
+//  Created by Andie Le Quiniat on 18/08/13.
+//  Copyright (c) 2013 Andie Le Quiniat. All rights reserved.
+//
+
+#import "TEThirdViewController.h"
+
+@interface TEThirdViewController ()
+
+@end
+
+@implementation TEThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Third", @"Third");
+        self.tabBarItem.image = [UIImage imageNamed:@"first"];    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
