5c31701385baa3d8a7d6a0034399e6274d4dbd28
Kerouz <luc.lequiniat@me.com>
2013-08-18 15:49:49 +1000

    minor alignment change

--- TabExample/TESimpleViewController.m
+++ TabExample/TESimpleViewController.m
@@ -16,7 +16,8 @@
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithNibName:nibNameOrNil
+                           bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
     }
